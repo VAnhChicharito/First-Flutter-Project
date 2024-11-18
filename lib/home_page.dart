6dbebea34134e3e065e6e2a552270de2d21b7fe3
anhvv1 <anhvv1@vmogroup.com>
2024-11-18 10:36:56 +0700

    testtt

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -63,7 +63,7 @@ class _HomePageState extends State<HomePage> {
                   }
                 });
               },
-              child: Text('XXXXXX'),
+              child: Text('YYYYYY'),
             ),
             Visibility(
               visible: _result.isNotEmpty,
